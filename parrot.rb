e171b038b8cc264e94c350cd965ce21bde716928
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-07 02:27:03 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,23 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
-def parrot
-  puts "Squawk!"
+def parrot(phrase = "Squawk!")
 end
-parrot
+parrot("phrase")
 
 # returns the phrase
-def parrot
-  1.times {puts "Squawk!"}
-end
- 
- def parrot
-   "Squawk!"
- end
-   
-   def parrot(phrase)
-     puts "Pretty bird!"
-end 
-
-def parrot(phrase)
-  phrase = "Squawk!"
-end
-parrot("Pretty bird!")
