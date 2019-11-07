4323755e79a595f49f65950652294ac404a5f8d7
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-07 02:04:37 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,18 @@
 # Create method `parrot` that outputs a given phrase and
+def parrot
+  puts "Squawk!"
+end
+parrot
+
 # returns the phrase
+def parrot
+  1.times {puts "Squawk!"}
+end
+ 
+ def parrot
+   "Squawk!"
+ end
+   
+   def parrot(phrase)
+     puts "Squawk!"
+end 
